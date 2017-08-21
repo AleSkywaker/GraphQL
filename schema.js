fb3dca4803deddfa008a2fed797e827b7a47a47c
Alex Colombo <uricaine@hotmail.com>
2017-08-21 10:34:23 +0200

    files

---
+++ schema.js
@@ -0,0 +1,85 @@
+const axios = require('axios');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+
+//Hardcoded data
+/* const customers = [
+    {id: '1', name: 'Alex Colombo', email: 'alex@gmail.com', age: 28 },
+    {id: '2', name: 'George Lucas', email: 'george@gmail.com', age: 20 },
+    {id: '3', name: 'Jon Doe', email: 'jon@gmail.com', age: 26 }
+] */
+
+//Customer Type
+const CustomerType = new GraphQLObjectType({
+    name: 'Customer',
+    fields:()=>({
+        id:{type:GraphQLString},
+        name:{type:GraphQLString},
+        email:{type:GraphQLString},
+        age: {type: GraphQLInt}
+    })
+})
+
+//Root Query
+const RootQuery= new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        customer:{
+            type:CustomerType,
+            args:{
+                id:{type:GraphQLString}
+            },
+            resolve(parentValue, args){
+               /*  for(let i = 0; i< customers.length; i++){
+                    if(customers[i].id == args.id){
+                        return customers[i];
+                    }
+                } */
+                return axios.get('http://localhost:3000/customers/'+ args.id)
+                .then(res => res.data);
+
+            }
+        },
+        customers:{
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args){
+                return axios.get('http://localhost:3000/customers')
+                .then(res => res.data);
+            }
+        }
+    }
+});
+//Mutations
+const mutation = new GraphQLObjectType({
+    name : 'Mutation',
+    fields:{
+        addCustomer:{
+            type:CustomerType,
+            args:{
+                name:{type:new GraphQLNonNull(GraphQLString)},
+                email:{type:new GraphQLNonNull(GraphQLString)},
+                age:{type:new GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parentValue, args){
+                return axios.post('http://localhost:3000/customers', {
+                    name:args.name,
+                    email:args.email,
+                    age:args.age
+                })
+                .then(res => res.data)
+            }
+        }
+    }
+})
+
+
+module.exports = new GraphQLSchema({
+   query: RootQuery,
+   mutation
+});
